bb2d84ae3633b98b0bb8fdd6d276e7677b7c62e5
Mrerg7 <87950693+Mrerg7@users.noreply.github.com>
2026-05-19 16:16:20 -0700

    fix: remove @astrojs/sitemap to resolve build crash on Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://50goodsummers.com',
   integrations: [
     tailwind(),
-    sitemap({
-      changefreq: 'monthly',
-      priority: 0.9,
-    }),
   ],
   output: 'static',
   build: {
